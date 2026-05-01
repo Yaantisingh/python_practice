37b95824246d8533dfac5bbbb6e01508169d0b8c
Yaantisingh <singhyaanti@gmail.com>
2026-05-02 01:48:48 +0530

    code updated

--- FileHandling_2.py
+++ FileHandling_2.py
@@ -1,7 +1,7 @@
 #4. Read from a File
 #We used open in read mode and file.read to read and print to display
 
-f=open("E:/python_tutorial/data.txt")
-contents=f.read()
+file=open("E:/python_tutorial/data.txt")
+contents=file.read()
 print(contents)
-f.close()
+file.close()
--- StudentGrades_2.py
+++ StudentGrades_2.py
@@ -4,20 +4,43 @@
 #Update an existing student’s grade.
 #Print all student grades.
 
-#Create a dictionary where the keys are student names and the values are their grades
 grades = {
     "Ashish": "A",
     "Beena": "B",
-    "Kavita": "C",
-    "Diya": "B",
-    "Ansh": "A"
+    "Kavita": "C"
 }
 
-#Add a new student and grade
-grades['Seema']="B"
+print("Student Grades System")
+print("1. Add new student")
+print("2. Update student grade")
+print("3. Print all grades")
 
-#Update an existing student’s grade
-grades['Kavita']='A'
+choice = int(input("Enter your choice: "))
 
-#Print all student grade
-print(grades)
+# Add new student
+if choice == 1:
+    name = input("Enter student name: ")
+    grade = input("Enter grade: ")
+    grades[name] = grade
+    print("student added")
+    print(grades)
+
+# Update existing student
+elif choice == 2:
+    name = input("Enter student name to update:")
+    if name in grades:
+        grade = input("Enter new grade: ")
+        grades[name] = grade
+        print("Grade updated")
+        print(grades)
+    else:
+        print("Student not found")
+
+# Print all students
+elif choice == 3:
+    print("All Student Grades:")
+    print(grades)
+
+# Invalid choice
+else:
+    print("Invalid choice")
