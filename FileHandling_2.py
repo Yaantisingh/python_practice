3d648af9d214360b32480aa4d6908a74835e4ef2
Yaantisingh <singhyaanti@gmail.com>
2026-05-02 01:53:27 +0530

    final

--- FileHandling_2.py
+++ FileHandling_2.py
@@ -1,7 +1,7 @@
 #4. Read from a File
 #We used open in read mode and file.read to read and print to display
 
-file=open("E:/python_tutorial/data.txt")
+file=open("E:/python_tutorial/data.txt",'r')
 contents=file.read()
 print(contents)
 file.close()
